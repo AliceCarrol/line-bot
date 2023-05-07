7d267e3df90bbbc9aafc6d245fcc42732b857268
AliceCarrol <acquaprogram.tf@gmail.com>
2023-05-07 19:32:48 +0900

    Add Chat GPT function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const line = require('@line/bot-sdk');
 const express = require('express');
+const { Configuration, OpenAIApi } = require("openai");
 
 // create LINE SDK config from env variables
 const config = {
@@ -11,6 +12,12 @@ const config = {
 
 // create LINE SDK client
 const client = new line.Client(config);
+
+// create Chat GPT client
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 // create Express app
 // about Express itself: https://expressjs.com/
@@ -29,14 +36,19 @@ app.post('/callback', line.middleware(config), (req, res) => {
 });
 
 // event handler
-function handleEvent(event) {
+async function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: event.message.text}],
+  });
+
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  const echo = { type: 'text', text: completion.data.choices[0].message.content };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
